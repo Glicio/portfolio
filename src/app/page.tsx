3682a4b0543bc5c47f5af8250b77ac69bca923f1
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-08-06 16:05:33 -0300

    migrating to app router

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { type Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+    title: 'Home',
+    description: 'The main page of my portfolio',
+}
+
+export default function Page() {
+    return (
+        <>
+          main page
+        </>
+    );
+}
+
